c7ad3b700af73a16f74a4f9673f750a83860e636
David Rodriguez <alu0101120645@ull.edu.es>
2020-05-07 11:46:11 +0100

    Entrega jueves

--- practica-widget/js/index.js
+++ practica-widget/js/index.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
+
+    const boton1 = document.querySelector("#acordeon1");
+    const aria = document.querySelector("#acordeon1").getAttribute("aria-extended");
+    boton1.onclick = () => {
+        if (aria === "false") {
+            document.querySelector("#acordeon1").setAttribute("aria", "true");
+        }
+    }
+
     var instances = M.Collapsible.init(elems, options);
     instances.open(3);
   });
