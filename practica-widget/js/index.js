c2e527bdf750d2c6d6f6487bcbc40e2ab3d6b237
David Rodriguez <alu0101120645@ull.edu.es>
2020-05-12 18:54:19 +0100

    widget a medias

--- practica-widget/js/index.js
+++ practica-widget/js/index.js
@@ -1,20 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
+ 
 
-    const boton1 = document.querySelector("#acordeon1");
-    boton1.onclick = () => {
-        const body1 = document.querySelector("#body1").getAttribute("hidden");
-        if (body1 === "false" || body1 === "") {
-            document.querySelector("#body1").setAttribute("hidden", "true");
-        }else{
-            document.querySelector("#body1").setAttribute("hidden", "false");
-        }
-    }
-
-    var instances = M.Collapsible.init(elems, options);
-    instances.open(3);
-  });
-
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
